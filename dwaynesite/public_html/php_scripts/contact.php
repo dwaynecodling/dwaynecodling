2e3d60def9284f817ffa1d478c6b2789974a7b64
Daryl <darylcecile@gmail.com>
2020-06-09 15:53:33 +0100

    added basic PHP routing structure

---
+++ dwaynesite/public_html/php_scripts/contact.php
@@ -0,0 +1,55 @@
+<?php
+
+// TODO change this
+define("RECAPTCHA_V3_SECRET_KEY", '6Lf5DgEVAAAAABvIlQM1XngklKvnoclH76bgNhS5');
+
+function outputResult(bool $isSuccessful, string $message){
+    header("Content-Type: application/json");
+    echo json_encode([
+        "Success" => $isSuccessful,
+        "Message" => $message
+    ]);
+}
+
+function getPost($key, $fallback=""){
+    return isset($_POST[$key]) ? $_POST[$key] : $fallback;
+}
+
+if (isset($_POST["email"])) {
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
+} else {
+    outputResult(false, "Email address is missing");
+    exit;
+}
+
+$token = getPost("token");
+$action = getPost("action");
+$name = getPost("name");
+$message = getPost("message");
+
+// Verify captcha
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL,"https://www.google.com/recaptcha/api/siteverify");
+curl_setopt($curl, CURLOPT_POST, 1);
+curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query([
+    "secret" => RECAPTCHA_V3_SECRET_KEY,
+    "response" => $token
+]));
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$result = curl_exec($curl);
+curl_close($curl);
+
+$response = json_decode($result, true);
+
+// Check reCaptcha response
+if($response["success"] == '1' && $response["action"] == $action && $response["score"] >= 0.5) {
+
+//    $h = "From: $email";
+//    mail("dwayne@wearejh.com", "Message from $name", $message, $h);
+
+    outputResult(true, "Message sent");
+} else {
+    // Possible spam, ignore email;
+    outputResult(false, "Message not sent (Possible spam)");
+}
---
+++ dwaynesite/public_html/views/homepage.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dwayne Codling</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body>
+<section class="page">
+    <?= \Nano\ViewHelper::getPartialContent("nav") ?>
+
+    <h1>Homepage</h1>
+
+    <?= \Nano\ViewHelper::getPartialContent("footer") ?>
+</section>
+
+</body>
+</html>
